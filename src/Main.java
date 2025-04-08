69c7584112860c82281fae4db61a292e85258d14
previsao do tempo <wbassplay@gmail.com>
2025-04-08 17:57:11 -0300

    dias da semana com switch case

--- src/Main.java
+++ src/Main.java
@@ -8,24 +8,33 @@ public  class Main {
         int x = sc.nextInt();
         String dia;
 
-        if (x == 1) {
-            dia = "domingo";
-        } else if (x == 2) {
-            dia = "segunda";
-        } else if (x == 3) {
-            dia = "terça";
-        } else if (x == 4) {
-            dia = "quarta";
-        } else if (x == 5) {
-            dia = "quinta";
-        } else if (x == 6) {
-            dia = "sexta";
-        } else if (x == 7) {
-            dia = "sabado";
-
-        } else {
-            dia = "valor invalido";
+        switch (x) {
+            case 1:
+                dia = "domingo";
+                break;
+            case 2:
+                dia = "segunda";
+                break;
+            case 3:
+                dia = "terça";
+                break;
+            case 4:
+                dia = "quarta";
+                break;
+            case 5:
+                dia = "quinta";
+                break;
+            case 6:
+                dia = "sexta";
+                break;
+            case 7:
+                dia = "sabado";
+                break;
+            default:
+                dia = "valor invalido";
         }
+
+
         System.out.println("dia da semana: " + dia)
 
         ;
